910d9c07a7c39b29cc025cf1285b15fabc4f4fc6
ychahbar19 <ychahbar19@gmail.com>
2023-11-02 16:57:04 +0100

    feat: Nuxt project boilerplate init

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,7 @@ import { defineVitestConfig } from 'nuxt-vitest/config';
 export default defineVitestConfig({
 	test: {
 		environment: 'node',
+		passWithNoTests: true,
 		deps: {
 			experimentalOptimizer: true,
 		},
